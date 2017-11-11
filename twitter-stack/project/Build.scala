9d24e7150685eb7649142c00aabe2fb04cf4a4c0
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-11-10 23:13:37 -0200

    Removed unsed file

--- twitter-stack/project/Build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-object ForkQuillBuild extends Build {
-
-  lazy val root = Project("root", file(".")) dependsOn(soundPlayerProject)
-  lazy val soundPlayerProject =
-       RootProject(uri("git://github.com/alvinj/SoundFilePlayer.git"))
-
-}
